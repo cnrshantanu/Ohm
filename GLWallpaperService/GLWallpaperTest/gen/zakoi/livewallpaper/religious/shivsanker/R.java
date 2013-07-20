42a20b7f06f2280eb48e235624aabcd039323fb1
Shantanu Das <cnrshantanu@gmail.com>
2013-07-20 14:00:15 +0530

    Almost done

--- GLWallpaperService/GLWallpaperTest/gen/zakoi/livewallpaper/religious/shivsanker/R.java
+++ GLWallpaperService/GLWallpaperTest/gen/zakoi/livewallpaper/religious/shivsanker/R.java
@@ -17,12 +17,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int background1=0x7f020000;
-        public static final int background2=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int icon=0x7f020003;
-        public static final int ohm=0x7f020004;
-        public static final int ohm1=0x7f020005;
+        public static final int background=0x7f020000;
+        public static final int background1=0x7f020001;
+        public static final int background2=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int ohm=0x7f020005;
         public static final int shiva=0x7f020006;
     }
     public static final class layout {
--- GLWallpaperService/GLWallpaperTest/src/zakoi/livewallpaper/religious/shivsanker/BackGroundController.java
+++ GLWallpaperService/GLWallpaperTest/src/zakoi/livewallpaper/religious/shivsanker/BackGroundController.java
@@ -11,9 +11,10 @@ import android.content.res.Resources;
  */
 public class BackGroundController {
 	
-	private	int				C_NO_LAYERS = 3;
-	private float			m_rotAngle	= 0;
-	private NeelkanthTile[] m_layers 	= new NeelkanthTile[C_NO_LAYERS];	
+	private	int				C_NO_LAYERS 		= 3;
+	private float			m_rotAngle			= 0;
+	private float 			m_snakeRotAngle 	= 0;
+	private NeelkanthTile[] m_layers 			= new NeelkanthTile[C_NO_LAYERS];	
 	
 	public BackGroundController(){
 		
@@ -23,9 +24,10 @@ public class BackGroundController {
 	
 	public void initGraphics(GL10 gl,Resources _resource){
 		
-		m_layers[0].loadGLTexture(gl, _resource,R.drawable.background1);
+		m_layers[0].loadGLTexture(gl, _resource,R.drawable.background);
 		m_layers[1].loadGLTexture(gl, _resource,R.drawable.background2);
 		m_layers[2].loadGLTexture(gl, _resource,R.drawable.shiva);		
+		//m_layers[3].loadGLTexture(gl, _resource,R.drawable.snake);
 									
 	}
 	
@@ -37,6 +39,10 @@ public class BackGroundController {
 		m_rotAngle += 0.4f;
 		if(m_rotAngle>360)
 			m_rotAngle -= 360;
+		
+		m_snakeRotAngle += 0.1f;
+		if(m_snakeRotAngle>180)
+			m_snakeRotAngle -= 180;
 	}
 	
 	public void draw(GL10 gl){
@@ -72,7 +78,20 @@ public class BackGroundController {
 		else
 			gl.glScalef(28, 30, 0);
 		m_layers[2].draw(gl);
-		gl.glPopMatrix(); 
+		gl.glPopMatrix();
+//		gl.glDisable(GL10.GL_BLEND);
+//		gl.glColor4f(1f, 1f, 1f,1f);
+//		gl.glPushMatrix();
+//		gl.glTranslatef(-7.5f,-2.5f,-70);
+//		gl.glRotatef((float)Math.sin(m_snakeRotAngle) * 5, 0, 0, 1);
+//		gl.glTranslatef(0f,-0.8f,0);
+//		if(!NeelkanthTile.istabletDevice)
+//			gl.glScalef(1.65f, 3.2f, 0f);
+//		else
+//			gl.glScalef(28, 30, 0);
+//		m_layers[3].draw(gl);
+//		gl.glPopMatrix();
+//		gl.glEnable(GL10.GL_BLEND);
 		
 		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
 		gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
